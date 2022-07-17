4a62b615b058c48c8ebbda2e7c259554be7147ea
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2022-07-17 21:45:32 +0530

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="write">Write</div>
+      <div className="appName">Sqeak</div>
+      <div className="userName">Aditya Deo</div>
+    </div>
+  );
+};
+
+export default Navbar;
