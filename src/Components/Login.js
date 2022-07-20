91a6878050d61d35084b232971babe01e010707d
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2022-07-20 08:55:42 +0530

    design login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import "./Login.css";
 const Login = (props) => {
-  return <div>Login</div>;
+  const handleLogin = (e) => {
+    console.log("login button clicked");
+  };
+  return (
+    <div className="landingPage">
+      <div className="heading">
+        <div className="premise">
+          <span>Put your hands in the air and</span>
+        </div>
+        <div className="punchline">Sqeak</div>
+      </div>
+      <div className="loginButton">
+        <button onClick={handleLogin} className="loginSubmit" type="button">
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
